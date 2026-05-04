84c80dbc56b0540cc8de9ba4e3a79d90320d0001
Katyayani Kumari <katyayanitiwari2021@gmail.com>
2026-05-04 22:31:24 +0530

    Time: 0 ms (100%), Space: 43.7 MB (51.87%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for(int i= 0;i <= n-1 ; i++){
+            for(int j=i; j <= n-1; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+                
+                
+            }
+        }
+        for(int i=0;i<= n-1 ;i++){
+            
+                int left = 0;
+                int right = n-1;
+                while(left < right){
+                    int temp = matrix[i][left];
+                    matrix[i][left]=matrix[i][right];
+                    matrix[i][right]=temp;
+                    left++ ;
+                    right-- ;
+                }
+
+                
+               
+
+
+            
+
+        }
+        return ;
+
+        
+    }
+}
