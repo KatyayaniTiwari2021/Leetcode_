68eab80fc096b3cb7ed034009caf84bebc424dfc
Katyayani Kumari <katyayanitiwari2021@gmail.com>
2026-05-07 00:06:37 +0530

    Time: 10 ms (25.24%), Space: 124.7 MB (60.34%) - LeetHub

---
+++ 1861-rotating-the-box/1861-rotating-the-box.java
@@ -0,0 +1,59 @@
+class Solution {
+    public char[][] rotateTheBox(char[][] boxGrid) {
+        int n = boxGrid.length;
+        int m = boxGrid[0].length;
+
+        char[][] ans  = new char[m][n];
+
+        //create the transpose of the input grid in ans
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+
+                ans[j][i] = boxGrid[i][j];
+                
+            }
+        }
+
+        //reverse each row in the transpose grid to complete the 90degree rotation
+
+        for(int i = 0; i < m; i++){
+
+            for(int j = 0; j < n/2; j++){
+                char  temp = ans[i][j];
+                ans[i][j] = ans[i][n - 1 - j];
+                ans[i][n - 1 - j] = temp;
+            }
+
+        }
+
+        //har baar lowest empty row ka track rkhenge
+
+        for(int j = 0; j < n; j++){
+
+            int lowestEmpty = m - 1;
+
+            //process each cell from bottom to top
+
+            for(int i = m - 1; i >= 0; i--){
+
+                //found a stone - let it fall to the lowest empty cell
+
+                if(ans[i][j] == '#'){
+                    ans[i][j] = '.';
+                    ans[lowestEmpty][j] = '#';
+
+                    lowestEmpty--;
+
+                }
+                //found an obstacle - reset lowestEmpty to row directly above it
+                if(ans[i][j] == '*'){
+                    lowestEmpty = i - 1;
+                }
+            }
+        }
+
+        return ans;
+        
+    }
+}
