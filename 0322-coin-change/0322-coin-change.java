2b50b26e689b6ca62895e727a664d99e44b7e6ac
Katyayani Kumari <katyayanitiwari2021@gmail.com>
2026-07-03 09:54:56 +0530

    Time: 15 ms (82.05%), Space: 47.1 MB (19.16%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int[][] dp = new int[n][amount+1];
+        for(int i =0; i < n; i++){
+            dp[i][0] = 0;
+        }
+        for(int j = 1; j <= amount; j++){
+            if(j % coins[0] == 0){
+                dp[0][j] = j/coins[0];
+            }else{
+                dp[0][j] = (int)1e9;
+            }
+
+        }
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j <= amount; j++){
+                int nonTake = dp[i-1][j];
+
+                int take = (int)1e9;
+                if(coins[i] <= j){
+                take = 1 + dp[i][j - coins[i]];
+                }
+               dp[i][j] = Math.min(take, nonTake);
+
+            }
+        }
+        return dp[n-1][amount] == (int)1e9 ? -1 : dp[n-1][amount];
+        
+    }
+    
+    
+   
+}
