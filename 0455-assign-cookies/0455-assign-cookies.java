5edc43fe7b9eae42c46e0cd6d1bfcfa9a22b7dcf
Katyayani Kumari <katyayanitiwari2021@gmail.com>
2026-07-13 10:33:51 +0530

    Time: 13 ms (98.97%), Space: 51 MB (96.02%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+
+        int n = g.length;
+        int m = s.length;
+
+        int count = 0;
+
+        int i = 0;
+        int j = 0;
+
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        while(i < n && j < m){
+            if(g[i] <= s[j]){
+                count++;
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+
+        }
+        return count;
+        
+    }
+}
