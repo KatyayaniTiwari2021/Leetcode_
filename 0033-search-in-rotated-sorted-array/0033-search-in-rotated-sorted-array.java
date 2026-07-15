9483537e185b5eb50027792de949c02ef7ee6a53
Katyayani Kumari <katyayanitiwari2021@gmail.com>
2026-07-15 13:09:27 +0530

    Time: 0 ms (100%), Space: 43.8 MB (46.76%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,56 +1,76 @@
 class Solution {
     public int search(int[] nums, int target) {
-     int pivot=findPivot(nums);
 
-     if(pivot==-1){
-        return binarySearch(nums,target,0,nums.length-1);
-     }
-     if(nums[pivot]==target){
-        return pivot;
-     }
-     if(target>=nums[0]){
-        return binarySearch(nums,target,0,pivot-1);
-     }
-     return binarySearch(nums,target,pivot+1,nums.length-1);
+        int n = nums.length;
 
+        int piv = pivot(nums);
 
+        if(piv == -1){
+
+            return bs(nums, target, 0, n-1);
+
+        }
+
+        if(nums[piv] == target){
+            return piv;
+        }
+
+        if(target >= nums[0]){
+
+            return bs(nums, target, 0, piv-1);
+
+        }
+
+        return bs(nums, target, piv + 1, n-1);
+
+       
+        
         
     }
-    static int binarySearch(int[] arr,int target,int start,int end){
-        while(start<=end){
-            int mid=(start+end)/2;
-            if(arr[mid]<target){
-                start=mid+1;
-            }else if(arr[mid]>target){
-                end=mid-1;
-            }else{
+
+    public int bs(int[] nums, int target, int low, int high){
+        while(low <= high){
+            int mid = low + (high - low)/2;
+
+            if(nums[mid] < target){
+                low = mid + 1;
+            }
+            else if(nums[mid] > target){
+                high = mid - 1;
+            }
+            else {
                 return mid;
             }
         }
-        return -1;
-    }
-    static int findPivot(int[] nums){
-        int start=0;
-        int end= nums.length-1;
-        while (start<=end){
-            int mid=(start+end)/2;
-            if(mid<end && nums[mid]>nums[mid+1]){
-                return mid;
-            }else if(mid>start && nums[mid]<nums[mid-1]){
-                return mid-1;
-            }
-            // else {
-            //     pivot=nums[mid+1];
-            // }
-            if(nums[start]>=nums[mid]){
-                end=mid-1;
-            }else{
-                start=mid+1;
-            }
-        }
+
         return -1;
 
     }
-   
-   
+
+    private int pivot(int[] nums){
+        int low = 0;
+        int high = nums.length - 1;
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+
+            if(mid < high && nums[mid] > nums[mid+1]){
+                return mid ;
+            }
+            else if(mid > low && nums[mid] < nums[mid-1]){
+                return mid - 1;
+            }
+
+            if(nums[low] >= nums[mid]){
+
+                high = mid - 1;
+
+            }else {
+                low = mid + 1;
+            }
+
+            
+        }
+        return -1;
+    }
 }
