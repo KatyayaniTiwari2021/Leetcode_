0e84d6f9fb61233d083ddeb5a4f4fbcc348a51c7
Katyayani Kumari <katyayanitiwari2021@gmail.com>
2026-08-01 00:48:28 +0530

    Time: 63 ms (13.01%), Space: 48.1 MB (32.08%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -1,23 +1,33 @@
 class Solution {
     public int minimumPushes(String word) {
-        Integer[] freq = new Integer[26];
-        for(int i = 0 ; i < 26 ; i++){
-            freq[i] = 0;
-        }
-        for(int i = 0 ; i < word.length(); i++){
-            char ch = word.charAt(i);
-            freq[ch-'a']++;
-        }
-        Arrays.sort(freq,Collections.reverseOrder());
-        int ans = 0 ;
 
-        for(int i = 0 ; i < 26 ; i++){
-            int Freq = freq[i];
-            int press = i/8 + 1;
-            ans += Freq*press;
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(char ch : word.toCharArray()){
+
+            map.put(ch, map.getOrDefault(ch, 0)+1);
+
+        }
+
+        List<Map.Entry<Character, Integer>> list = new ArrayList<>(map.entrySet());
+        list.sort((a, b) -> b.getValue() - a.getValue());
+
+        int ans = 0;
+        int n = list.size();
+
+        for(int i = 0; i < n; i++){
+
+            int value = list.get(i).getValue();
+
+            int push = i / 8 + 1;
+
+            ans += value * push;
 
         }
         return ans;
+
+
+        
         
     }
 }
