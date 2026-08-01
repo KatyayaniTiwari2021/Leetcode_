872902f4605fe84e5f6cb0eb09fa152774d4f6dc
Katyayani Kumari <katyayanitiwari2021@gmail.com>
2026-08-01 23:51:43 +0530

    Time: 65 ms (28.69%), Space: 42.9 MB (41.06%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,31 +1,17 @@
 class Solution {
     public boolean predictTheWinner(int[] nums) {
 
-        int total = 0;
-
-        for(int num : nums){
-            total += num;
-        }
-
-        int player1 = solve(0, nums.length - 1, nums);
-        int player2 = total - player1;
-
-        if(player1 > player2 || player1 == player2){
-            return true;
-        }
-
-        return false;
+        return helper(0, nums.length - 1, nums) >= 0;
         
     }
 
-    private int solve(int i , int j, int[] nums){
-        if(i > j) return 0;
-
+    private int helper(int i, int j, int[] nums){
         if(i == j) return nums[i];
 
-        int first = nums[i] + Math.min(solve(i + 2, j, nums), solve(i + 1, j -1, nums));
-        int second = nums[j] + Math.min(solve(i, j - 2, nums), solve(i + 1,j-1,nums));
+        int left = nums[i]  - helper(i + 1, j, nums);
 
-        return Math.max(first, second);
+        int right = nums[j]  - helper(i, j - 1, nums);
+
+        return Math.max(left, right);
     }
 }
